c55e410c1595bcffa0cffa2c2efa25d9e290589c
Daniel Compton <desk@danielcompton.net>
2020-05-19 03:41:53 +1200

    Add batch fill namespace

---
+++ src/org/clojuriststogether/app/admin/batch_fill.clj
@@ -0,0 +1,34 @@
+(ns org.clojuriststogether.app.admin.batch-fill
+  (:require [honeysql.core :as sql]
+            [clojure.java.jdbc :as jdbc])
+  (:import (com.stripe.model Customer Subscription)
+           (com.stripe Stripe)))
+
+(defn create-customer-and-subscription [db {:keys [id email name subscription_plan]}]
+  (let [customer (Customer/create {"email" email
+                                   "name" name})
+        customer-id (.getId customer)
+        _ (Subscription/create {"customer" customer-id
+                                "items" [{"plan" subscription_plan}]
+                                "trial_period_days" 30})
+        member (->> {:update :members
+                     :set {:stripe_customer_id customer-id}
+                     :where [:= :id id]}
+                    (sql/format)
+                    (jdbc/execute! db))]))
+
+(defn find-all-backfills [db]
+  (->> {:select [:*]
+        :from [:members]
+        :where [:= :stripe_customer_id "missing"]}
+       (sql/format)
+       (jdbc/query db)))
+
+(defn update-all-customers [db]
+  ; heroku run echo \$JDBC_DATABASE_URL
+  ;(set! Stripe/apiKey "")
+
+  (let [members (find-all-backfills db)]
+    (doseq [member members]
+      (prn "Updating" (:email member))
+      (create-customer-and-subscription db member))))
