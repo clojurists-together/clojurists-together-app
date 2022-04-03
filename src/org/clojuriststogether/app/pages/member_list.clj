2111b686904fe0a7ba21cb43ea0baec95b4cc645
Daniel Compton <desk@danielcompton.net>
2022-04-04 09:50:26 +1200

    Add new prices

--- src/org/clojuriststogether/app/pages/member_list.clj
+++ src/org/clojuriststogether/app/pages/member_list.clj
@@ -6,8 +6,7 @@
             [cheshire.core :as cheshire]))
 
 (def plans-mapping
-  {
-   ;; Production
+  {;; Production
    "plan_GYMOB8MprRabK5" "contributing"
    "plan_GYMObrXegHTSlE" "contributing"
    "plan_GYMO2CgIuB3Np5" "contributing"
@@ -23,8 +22,10 @@
    "plan_GYMFD9fLVTXvNH" "transduce"
    "plan_GYMFuEYbKe7nhC" "transduce"
    "price_1JNPd4GYL0HLUlSLxdt1vJz8" "transduce"
+   "price_1JjavuGYL0HLUlSLHKnx0qWD" "transduce"
    "plan_GYMEwKK79GIrt1" "filter"
    "plan_GYMD6AQHDFhQGw" "filter"
+   "price_1KNjkRGYL0HLUlSLlJ6VD4It" "filter"
    "plan_GYMDwLn4mu4TVa" "into"
    "plan_GYMCd3ySx0VVXt" "into"
    "plan_GYMCg0s8rWxvt3" "cons"
@@ -38,8 +39,7 @@
    "plan_HGLFgYEkItJGf8" "contributing"
    "plan_HGLEqqukKXBaXh" "contributing"
    "plan_GY8nEzNqbLlALp" "developer"
-   "plan_GY8ntpqaHEUlza" "developer"
-   })
+   "plan_GY8ntpqaHEUlza" "developer"})
 
 (defn get-members [db]
   (->> {:select [:person_name :organization_name :organization_url
@@ -52,17 +52,17 @@
        (jdbc/query db)
        (map (fn [member]
               (merge
-                (if (= "company" (:member_type member))
-                  {:name (:organization_name member)
-                   :member_type (:member_type member)
-                   :url (:organization_url member)
-                   :logo (:logo_slug member)}
-                  {:name (:person_name member)
-                   :member_type (:member_type member)})
-                {:level (get plans-mapping (:subscription_plan member))
-                 :founding (:founding_member member)}
-                (when-let [tagline (:tagline member)]
-                  {:tagline tagline}))))
+               (if (= "company" (:member_type member))
+                 {:name (:organization_name member)
+                  :member_type (:member_type member)
+                  :url (:organization_url member)
+                  :logo (:logo_slug member)}
+                 {:name (:person_name member)
+                  :member_type (:member_type member)})
+               {:level (get plans-mapping (:subscription_plan member))
+                :founding (:founding_member member)}
+               (when-let [tagline (:tagline member)]
+                 {:tagline tagline}))))
        ;; Don't think this removes all non-active members yet
        (filter :level)))
 
@@ -82,8 +82,8 @@
     {:name :member-list
      :get {:handler (fn [_req]
                       (->
-                        {:members (get-members db)
-                         :member_stats (get-member-stats db)}
-                        (cheshire/generate-string {:pretty true})
-                        (response/ok)
-                        (response/content-type "application/json")))}}]])
+                       {:members (get-members db)
+                        :member_stats (get-member-stats db)}
+                       (cheshire/generate-string {:pretty true})
+                       (response/ok)
+                       (response/content-type "application/json")))}}]])
